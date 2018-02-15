450ebb8b42ce51317d0deb48057b405cd526bbd0
William Fedus <liam.fedus@gmail.com>
2018-02-15 11:34:29 -0500

    Reformatting changes

--- train.py
+++ train.py
@@ -27,29 +27,51 @@ from utils import plotting
 # -----------------------------------------------------------------------------
 parser = argparse.ArgumentParser()
 # data I/O
-parser.add_argument('-o', '--save_dir', type=str, default='data', help='Location for parameter checkpoints and samples')
-parser.add_argument('-t', '--save_interval', type=int, default=20, help='Every how many epochs to write checkpoint/samples?')
-parser.add_argument('-r', '--load_params', dest='load_params', action='store_true', help='Restore training from previous model checkpoint?')
+parser.add_argument('-o', '--save_dir', type=str, default='data', 
+        help='Location for parameter checkpoints and samples')
+parser.add_argument('-d', '--train_data', type=str,
+        default='/u/alitaiga/repositories/count-based-exploration/data/montezuma_color.h5', 
+        help='Location for HDF5 training data.')
+parser.add_argument('-t', '--save_interval', type=int, default=20,
+        help='Every how many epochs to write checkpoint/samples?')
+parser.add_argument('-r', '--load_params', dest='load_params', 
+        action='store_true', help='Restore training from previous model checkpoint?')
 # model
-parser.add_argument('-q', '--nr_resnet', type=int, default=5, help='Number of residual blocks per stage of the model')
-parser.add_argument('-n', '--nr_filters', type=int, default=160, help='Number of filters to use across the model. Higher = larger model.')
-parser.add_argument('-m', '--nr_logistic_mix', type=int, default=10, help='Number of logistic components in the mixture. Higher = more flexible model')
-parser.add_argument('-z', '--resnet_nonlinearity', type=str, default='concat_elu', help='Which nonlinearity to use in the ResNet layers. One of "concat_elu", "elu", "relu" ')
-parser.add_argument('-c', '--class_conditional', dest='class_conditional', action='store_true', help='Condition generative model on labels?')
-parser.add_argument('-ed', '--energy_distance', dest='energy_distance', action='store_true', help='use energy distance in place of likelihood')
+parser.add_argument('-q', '--nr_resnet', type=int, default=5, 
+        help='Number of residual blocks per stage of the model')
+parser.add_argument('-n', '--nr_filters', type=int, default=160,
+        help='Number of filters to use across the model. Higher = larger model.')
+parser.add_argument('-m', '--nr_logistic_mix', type=int, default=10,
+        help='Number of logistic components in the mixture. Higher = more flexible model')
+parser.add_argument('-z', '--resnet_nonlinearity', type=str, default='concat_elu',
+        help='Which nonlinearity to use in the ResNet layers. One of "concat_elu", "elu", "relu" ')
+parser.add_argument('-c', '--class_conditional', dest='class_conditional',
+        action='store_true', help='Condition generative model on labels?')
+parser.add_argument('-ed', '--energy_distance', dest='energy_distance',
+        action='store_true', help='use energy distance in place of likelihood')
 # optimization
-parser.add_argument('-l', '--learning_rate', type=float, default=0.001, help='Base learning rate')
-parser.add_argument('-e', '--lr_decay', type=float, default=0.999995, help='Learning rate decay, applied every step of the optimization')
-parser.add_argument('-b', '--batch_size', type=int, default=16, help='Batch size during training per GPU')
-parser.add_argument('-u', '--init_batch_size', type=int, default=16, help='How much data to use for data-dependent initialization.')
-parser.add_argument('-p', '--dropout_p', type=float, default=0.5, help='Dropout strength (i.e. 1 - keep_prob). 0 = No dropout, higher = more dropout.')
-parser.add_argument('-x', '--max_epochs', type=int, default=5000, help='How many epochs to run in total?')
-parser.add_argument('-g', '--nr_gpu', type=int, default=1, help='How many GPUs to distribute the training across?')
+parser.add_argument('-l', '--learning_rate', type=float, default=0.001,
+        help='Base learning rate')
+parser.add_argument('-e', '--lr_decay', type=float, default=0.999995,
+        help='Learning rate decay, applied every step of the optimization')
+parser.add_argument('-b', '--batch_size', type=int, default=16,
+        help='Batch size during training per GPU')
+parser.add_argument('-u', '--init_batch_size', type=int, default=16,
+        help='How much data to use for data-dependent initialization.')
+parser.add_argument('-p', '--dropout_p', type=float, default=0.5,
+        help='Dropout strength (i.e. 1 - keep_prob). 0 = No dropout, higher = more dropout.')
+parser.add_argument('-x', '--max_epochs', type=int, default=5000,
+        help='How many epochs to run in total?')
+parser.add_argument('-g', '--nr_gpu', type=int, default=1,
+        help='How many GPUs to distribute the training across?')
 # evaluation
-parser.add_argument('--polyak_decay', type=float, default=0.9995, help='Exponential decay rate of the sum of previous model iterates during Polyak averaging')
-parser.add_argument('-ns', '--num_samples', type=int, default=1, help='How many batches of samples to output.')
+parser.add_argument('--polyak_decay', type=float, default=0.9995,
+        help='Exponential decay rate of the sum of previous model iterates during Polyak averaging')
+parser.add_argument('-ns', '--num_samples', type=int, default=1,
+        help='How many batches of samples to output.')
 # reproducibility
-parser.add_argument('-s', '--seed', type=int, default=1, help='Random seed to use')
+parser.add_argument('-s', '--seed', type=int, default=1,
+        help='Random seed to use')
 args = parser.parse_args()
 print('input args:\n', json.dumps(vars(args), indent=4, separators=(',',':'))) # pretty print args
 
@@ -63,7 +85,7 @@ loss_fun = nn.discretized_mix_logistic_loss
 
 # initialize data loaders for train/test splits
 train_data = H5PYDataset(
-    '/u/alitaiga/repositories/count-based-exploration/data/montezuma_color.h5', which_sets=('train',), sources=('images',)
+        args.train_data, which_sets=('train',), sources=('images',)
 )
 stream = DataStream(train_data, iteration_scheme=SequentialScheme(train_data.num_examples, args.batch_size))
 obs_shape = (32, 32, 3)  # e.g. a tuple (32,32,3)
